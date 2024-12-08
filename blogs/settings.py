356d0fe43ae68c1193bc6cd4c69719b8b9af955b
Getaneh Biratu <mrgetaneh@gmail.com>
2024-12-08 20:25:34 +0000

    add heroku link to allowed hosts

--- blogs/settings.py
+++ blogs/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-53c7h7ix5=sgahzw*!=ovug-o@j#cmhrdmdj!_!i)#zwd_t(9!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["8000-getaneht-myblogs-1gj5hblmclj.ws.codeinstitute-ide.net",'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ["8000-getaneht-myblogs-1gj5hblmclj.ws.codeinstitute-ide.net",'localhost', '127.0.0.1', 'https://my-blog-application-6967878bdb6b.herokuapp.com/']
 
 
 CSRF_TRUSTED_ORIGINS = [
